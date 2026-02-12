d23edb035f7cd39128993e6a6d2ee20f051e9941
tk-44 <tannerklinge@u.boisestate.edu>
2026-02-12 13:20:57 -0700

    Add files via upload

---
+++ lab_04_materials/class/maxterm.v
@@ -0,0 +1,9 @@
+module maxterm (
+    input A, B, C, D,
+    output Y
+
+);
+
+assign Y = (B | C | D) & (~A | ~D) & (~B | ~D);
+
+endmodule
---
+++ lab_04_materials/class/minterm.v
@@ -0,0 +1,7 @@
+module minterm (
+    input A, B, C, D,
+    output Y
+);
+
+assign Y = (B & ~C & ~D) | (~A & ~B & D) | (C & ~D);
+endmodule
